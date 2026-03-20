9c72a0b2c9ac35fe2352d24f25ba8c296b040999
Hossein <hosseinsherkat@gmail.com>
2026-03-20 14:24:23 +0100

    Tesing: uncomment using json file, see if i can do it automaitcally
    go back one commit if things dont work out

--- MainCodeHossein.py
+++ MainCodeHossein.py
@@ -8,6 +8,7 @@ from audiocraft.models import AudioGen
 import transformers.utils.import_utils as import_utils
 import_utils._torch_available = True 
 import warnings
+import json
 from typing import Dict, List, Optional, Any
 
 
@@ -223,81 +224,81 @@ if __name__ == "__main__":
         # Look up the ID; if not found, use a safe default
         return vgg_lookup.get(youtube_id, None)
 
-    results = {
-            '--XInAaMS6k.npy': {'-C6cbmMaENE.npy': 0.6466576988106167,
-                        '-8KFpJHyspw.npy': 0.14904139426690938,
-                        '-03N_1zOM4E.npy': 0.10659110957951061,
-                        '-KQ7U3gS1wQ.npy': 0.05174921559957428,
-                        '-HWoFxKmyyo.npy': 0.04596058174338897}
-    ,
+    # results = {
+    #         '--XInAaMS6k.npy': {'-C6cbmMaENE.npy': 0.6466576988106167,
+    #                     '-8KFpJHyspw.npy': 0.14904139426690938,
+    #                     '-03N_1zOM4E.npy': 0.10659110957951061,
+    #                     '-KQ7U3gS1wQ.npy': 0.05174921559957428,
+    #                     '-HWoFxKmyyo.npy': 0.04596058174338897}
+    # ,
 
 
 
-    '-0gYWIOfqdM.npy': {'-0gYWIOfqdM.npy': 0.6202382082358212,
-                        '-4yCSY_5Zns.npy': 0.31029253698116976,
-                        '-D7Od7iYq0A.npy': 0.04162212451171154,
-                        '-A-xb-P-WxQ.npy': 0.02028793443981568,
-                        '-HtBJbsbeHo.npy': 0.0075591958314815974}
-    ,
+    # '-0gYWIOfqdM.npy': {'-0gYWIOfqdM.npy': 0.6202382082358212,
+    #                     '-4yCSY_5Zns.npy': 0.31029253698116976,
+    #                     '-D7Od7iYq0A.npy': 0.04162212451171154,
+    #                     '-A-xb-P-WxQ.npy': 0.02028793443981568,
+    #                     '-HtBJbsbeHo.npy': 0.0075591958314815974}
+    # ,
 
 
 
-    '-3M-k4nIYIM.npy': {'-9whJW7BUSU.npy': 0.3196014880596852,
-                        '-HxQ9AoyRmY.npy': 0.24735819844202067,
-                        '-60vY5Xw1qE.npy': 0.15602753270196487,
-                        '-9vw5ZzChT0.npy': 0.14684911321636024,
-                        '-3MNphBfq_0.npy': 0.13016366757996897}
-    ,
+    # '-3M-k4nIYIM.npy': {'-9whJW7BUSU.npy': 0.3196014880596852,
+    #                     '-HxQ9AoyRmY.npy': 0.24735819844202067,
+    #                     '-60vY5Xw1qE.npy': 0.15602753270196487,
+    #                     '-9vw5ZzChT0.npy': 0.14684911321636024,
+    #                     '-3MNphBfq_0.npy': 0.13016366757996897}
+    # ,
 
 
 
-    '-4ItJ9yTz_c.npy': {'-AioliAg12U.npy': 0.5865218525077042,
-                        '-NPu34as_OY.npy': 0.18724029723388622,
-                        '-6ZEGCtBKqs.npy': 0.10033209712998142,
-                        '-Gbohom8C4Q.npy': 0.08084076879787816,
-                        '-62pV95k9O0.npy': 0.045064984330550145}
-    ,
+    # '-4ItJ9yTz_c.npy': {'-AioliAg12U.npy': 0.5865218525077042,
+    #                     '-NPu34as_OY.npy': 0.18724029723388622,
+    #                     '-6ZEGCtBKqs.npy': 0.10033209712998142,
+    #                     '-Gbohom8C4Q.npy': 0.08084076879787816,
+    #                     '-62pV95k9O0.npy': 0.045064984330550145}
+    # ,
 
 
 
-    '-4o0jRbgHr4.npy': {'-NPu34as_OY.npy': 0.7716207865103134,
-                        '-CexapzRAPQ.npy': 0.1304685089335117,
-                        '-4o0jRbgHr4.npy': 0.05299553586319041,
-                        '-9wRxzJ5j_Y.npy': 0.022736311823520462,
-                        '-C8JU6yTJ40.npy': 0.022178856869464036}
-    ,
+    # '-4o0jRbgHr4.npy': {'-NPu34as_OY.npy': 0.7716207865103134,
+    #                     '-CexapzRAPQ.npy': 0.1304685089335117,
+    #                     '-4o0jRbgHr4.npy': 0.05299553586319041,
+    #                     '-9wRxzJ5j_Y.npy': 0.022736311823520462,
+    #                     '-C8JU6yTJ40.npy': 0.022178856869464036}
+    # ,
 
 
 
-    '-4rdRn-FRXo.npy': {'-Kc9P729mqM.npy': 0.36042281765518974,
-                        '-7XYw1VrN64.npy': 0.33141335961690443,
-                        '-MNP_aM09S8.npy': 0.1580765973593134,
-                        '-CCbu3r-1pc.npy': 0.0915348425868341,
-                        '-JdUSVmQq88.npy': 0.058552382781758214}
-    ,
+    # '-4rdRn-FRXo.npy': {'-Kc9P729mqM.npy': 0.36042281765518974,
+    #                     '-7XYw1VrN64.npy': 0.33141335961690443,
+    #                     '-MNP_aM09S8.npy': 0.1580765973593134,
+    #                     '-CCbu3r-1pc.npy': 0.0915348425868341,
+    #                     '-JdUSVmQq88.npy': 0.058552382781758214}
+    # ,
 
 
 
-    '-6lkiUAf_cQ.npy': {'-6lkiUAf_cQ.npy': 0.7625829829950478,
-                        '-ECRgvDx4xc.npy': 0.11362292998877925,
-                        '-3xhrOw45ss.npy': 0.0643086250163537,
-                        '-2JomCd5zzY.npy': 0.038541489457014064,
-                        '-FfFD4bbCEI.npy': 0.020943972542805226}
-    ,
+    # '-6lkiUAf_cQ.npy': {'-6lkiUAf_cQ.npy': 0.7625829829950478,
+    #                     '-ECRgvDx4xc.npy': 0.11362292998877925,
+    #                     '-3xhrOw45ss.npy': 0.0643086250163537,
+    #                     '-2JomCd5zzY.npy': 0.038541489457014064,
+    #                     '-FfFD4bbCEI.npy': 0.020943972542805226}
+    # ,
 
 
 
-    '-6VFTlZsft4.npy': {'-AltV1ftMk8.npy': 0.31224017893390565,
-                        '-EWyYYBHsbQ.npy': 0.2170137781686798,
-                        '-CcGuq0yoKo.npy': 0.20042663093106244,
-                        '-0NxpZlO348.npy': 0.18478375634990274,
-                        '-1EeNriiRN0.npy': 0.08553565561644937}
+    # '-6VFTlZsft4.npy': {'-AltV1ftMk8.npy': 0.31224017893390565,
+    #                     '-EWyYYBHsbQ.npy': 0.2170137781686798,
+    #                     '-CcGuq0yoKo.npy': 0.20042663093106244,
+    #                     '-0NxpZlO348.npy': 0.18478375634990274,
+    #                     '-1EeNriiRN0.npy': 0.08553565561644937}
 
-    }
+    # }
 
     # if u wanna use the json file: 
-    # with open("results.json", "r") as f:
-    #     results = json.load(f)
+    with open("results.json", "r") as f:
+        results = json.load(f)
 
     # --- 3. LOAD MODEL ONCE ---
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
